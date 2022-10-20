d69641a1a9d85c5a4b809b91a29699473c11551c
Unonna <elena.monich.em@gmail.com>
2022-10-20 15:17:31 +0700

    доработан скрипт TransparentModule
    доступно управление полями из редактора, а также запуск скрипта из контекстного меню

--- Assets/LearnMaterials 2/Scripts/TransparentModule.cs
+++ Assets/LearnMaterials 2/Scripts/TransparentModule.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1Cmm__cbik5J8aHAI6PPaAUmEMF3wAcNo3rpgzsYPzDM/edit?usp=sharing")]
 public class TransparentModule : MonoBehaviour
 {
+    [Header("Модуль")]
+    [SerializeField]
     private float changeSpeed;
 
     private float defaultAlpha;
@@ -19,6 +21,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Изменение цвета материала")]
     public void ActivateModule()
     {
         float target = toDefault ? defaultAlpha : 0;
@@ -27,6 +30,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = !toDefault;
     }
 
+    [ContextMenu("Возврат в изначальное состояние")]
     public void ReturnToDefaultState()
     {
         toDefault = true;
